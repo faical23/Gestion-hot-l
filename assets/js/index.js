bdeb1c9c052ed6ef55cfa2d0458c5a4383e8702b
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-05-06 05:32:16 +0000

    add readme file

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,9 @@ const client_data = (ID_client) => {
 }
 
 
-// let service = document.querySelector(".srevice_1_1");
 
-// service.addEventListener("click", () => {
-//     console.log(service.data);
-// })
 let img_our_srvice = document.querySelector(".img_our_service")
 
 function showDetails(type) {
-    console.log(type);
     img_our_srvice.src = `../assets/img/our_service/${type}.jpg `
 }
